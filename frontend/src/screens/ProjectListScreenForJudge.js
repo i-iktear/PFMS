7954e3d7b72631528adf20836150ee9c6f31742a
i-iktear <emon5aurk@gmail.com>
2021-09-08 17:45:26 +0600

    Added: sorting by session feature

--- frontend/src/screens/ProjectListScreenForJudge.js
+++ frontend/src/screens/ProjectListScreenForJudge.js
@@ -1,16 +1,26 @@
-import React, { useEffect } from "react";
-import { Button, Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Table, Row, Col, Dropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { listJudgeProjects } from "../actions/projectActions";
+import { getActiveSession } from "../actions/sessionActions";
+import { listSessions } from "../actions/sessionActions";
 
 const ProjectListScreenForJudge = ({ history }) => {
   const dispatch = useDispatch();
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const sessionActive = useSelector((state) => state.sessionActive);
+  const { session: activeSession, loading, error } = sessionActive;
+
+  const sessionList = useSelector((state) => state.sessionList);
+  const { sessions } = sessionList;
+
+  const [currSession, setCurrSession] = useState(activeSession?.name);
 
   const projectListForJudge = useSelector((state) => state.projectListForJudge);
   const {
@@ -22,6 +32,8 @@ const ProjectListScreenForJudge = ({ history }) => {
   useEffect(() => {
     if (userInfo) {
       dispatch(listJudgeProjects());
+      dispatch(getActiveSession());
+      dispatch(listSessions());
     } else {
       history.push("/login");
     }
@@ -29,17 +41,41 @@ const ProjectListScreenForJudge = ({ history }) => {
 
   return (
     <>
-      <center>
-        <h2>
-          Approved projects <i className="fas fa-hand-point-down"></i>{" "}
-        </h2>
-      </center>
+      <Row className="align-items-center">
+        <Col>
+          <h1>
+            <b>Approved projects</b> <i className="fas fa-hand-point-down"></i>{" "}
+          </h1>
+        </Col>
+        <Col className="text-right">
+          <Dropdown>
+            <Dropdown.Toggle className="my-3" id="dropdown-basic">
+              {currSession}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {sessions?.map(
+                (session) =>
+                  session?.name !== currSession && (
+                    <Dropdown.Item
+                      onClick={() => setCurrSession(session.name)}
+                      key={session._id}
+                    >
+                      {" "}
+                      {session.name}{" "}
+                    </Dropdown.Item>
+                  )
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
       <br />
 
-      {loadingProjects ? (
+      {loadingProjects || loading ? (
         <Loader />
-      ) : errorprojects ? (
-        <Message variant="danger">{errorprojects}</Message>
+      ) : errorprojects || error ? (
+        <Message variant="danger">{errorprojects || error}</Message>
       ) : (
         <Table striped hover resposnsive="true" className="table-sm">
           <thead>
@@ -55,32 +91,41 @@ const ProjectListScreenForJudge = ({ history }) => {
             </tr>
           </thead>
           <tbody>
-            {projects.map((project) => (
-              <tr key={project._id}>
-                <td>{project.name}</td>
-                <td>{project.category}</td>
-                <td>{project.teammembers}</td>
-                <td>{project.frontend}</td>
-                <td>{project.backend}</td>
-                <td>{project.database}</td>
+            {projects.map(
+              (project) =>
+                project.session === currSession && (
+                  <tr key={project._id}>
+                    <td>{project.name}</td>
+                    <td>{project.category}</td>
+                    <td>{project.teammembers}</td>
+                    <td>{project.frontend}</td>
+                    <td>{project.backend}</td>
+                    <td>{project.database}</td>
 
-                <td>
-                  {project.isApproved ? (
-                    <i className="fas fa-check" style={{ color: "green" }}></i>
-                  ) : (
-                    <i className="fas fa-times" style={{ color: "red" }}></i>
-                  )}
-                </td>
+                    <td>
+                      {project.isApproved ? (
+                        <i
+                          className="fas fa-check"
+                          style={{ color: "green" }}
+                        ></i>
+                      ) : (
+                        <i
+                          className="fas fa-times"
+                          style={{ color: "red" }}
+                        ></i>
+                      )}
+                    </td>
 
-                <td>
-                  <LinkContainer to={`/projects/${project._id}`}>
-                    <Button className="btn-sm" variant="info">
-                      Details
-                    </Button>
-                  </LinkContainer>
-                </td>
-              </tr>
-            ))}
+                    <td>
+                      <LinkContainer to={`/projects/${project._id}`}>
+                        <Button className="btn-sm" variant="info">
+                          Details
+                        </Button>
+                      </LinkContainer>
+                    </td>
+                  </tr>
+                )
+            )}
           </tbody>
         </Table>
       )}
--- frontend/src/screens/ProjectListScreenForModerator.js
+++ frontend/src/screens/ProjectListScreenForModerator.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Button, Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Row, Col, Dropdown, Table } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
@@ -9,11 +9,22 @@ import {
   deleteProject,
 } from "../actions/projectActions";
 
+import { getActiveSession } from "../actions/sessionActions";
+import { listSessions } from "../actions/sessionActions";
+
 const ProjectListScreenForModerator = ({ history }) => {
   const dispatch = useDispatch();
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const sessionActive = useSelector((state) => state.sessionActive);
+  const { session: activeSession, loading, error } = sessionActive;
+
+  const sessionList = useSelector((state) => state.sessionList);
+  const { sessions } = sessionList;
+
+  const [currSession, setCurrSession] = useState(activeSession?.name);
 
   const projectListForModerator = useSelector(
     (state) => state.projectListForModerator
@@ -34,6 +45,8 @@ const ProjectListScreenForModerator = ({ history }) => {
   useEffect(() => {
     if (userInfo) {
       dispatch(listModeratorProjects());
+      dispatch(getActiveSession());
+      dispatch(listSessions());
     } else {
       history.push("/login");
     }
@@ -47,18 +60,43 @@ const ProjectListScreenForModerator = ({ history }) => {
 
   return (
     <>
-      <center>
-        <h2>
-          <b> Submissions List </b> <i className="fas fa-hand-point-down"></i>{" "}
-        </h2>
-      </center>
+      <Row className="align-items-center">
+        <Col>
+          <h1>
+            <b> Submissions List </b> <i className="fas fa-hand-point-down"></i>{" "}
+          </h1>
+        </Col>
+        <Col className="text-right">
+          <Dropdown>
+            <Dropdown.Toggle className="my-3" id="dropdown-basic">
+              {currSession}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {sessions?.map(
+                (session) =>
+                  session?.name !== currSession && (
+                    <Dropdown.Item
+                      onClick={() => setCurrSession(session.name)}
+                      key={session._id}
+                    >
+                      {" "}
+                      {session.name}{" "}
+                    </Dropdown.Item>
+                  )
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
       <br />
+
       {loadingDelete && <Loader />}
       {errorDelete && <Message variant="danger"> {errorDelete} </Message>}
-      {loadingProjects ? (
+      {loading || loadingProjects ? (
         <Loader />
-      ) : errorprojects ? (
-        <Message variant="danger">{errorprojects}</Message>
+      ) : errorprojects || error ? (
+        <Message variant="danger">{errorprojects || error}</Message>
       ) : (
         <Table striped hover resposnsive="true" className="table-sm">
           <thead>
@@ -73,46 +111,57 @@ const ProjectListScreenForModerator = ({ history }) => {
             </tr>
           </thead>
           <tbody>
-            {projects.map((project) => (
-              <tr key={project._id}>
-                <td>{project.name}</td>
-                <td>{project.category}</td>
-                <td>{project.teammembers}</td>
+            {projects.map(
+              (project) =>
+                project.session === currSession && (
+                  <tr key={project._id}>
+                    <td>{project.name}</td>
+                    <td>{project.category}</td>
+                    <td>{project.teammembers}</td>
 
-                <td>
-                  {project.isApproved ? (
-                    <i className="fas fa-check" style={{ color: "green" }}></i>
-                  ) : (
-                    <i className="fas fa-times" style={{ color: "red" }}></i>
-                  )}
-                </td>
-                <td>
-                  <LinkContainer to={`/moderator/projects/${project._id}/edit`}>
-                    <Button variant="info" className="btn-sm">
-                      <i className="fas fa-edit"> </i>
-                    </Button>
-                  </LinkContainer>
-                </td>
-                <td>
-                  <Button
-                    variant="danger"
-                    className="btn-sm"
-                    onClick={() => {
-                      deleteHandler(project._id);
-                    }}
-                  >
-                    <i className="fas fa-trash"></i>
-                  </Button>
-                </td>
-                <td>
-                  <LinkContainer to={`/projects/${project._id}`}>
-                    <Button className="btn-sm" variant="info">
-                      Details
-                    </Button>
-                  </LinkContainer>
-                </td>
-              </tr>
-            ))}
+                    <td>
+                      {project.isApproved ? (
+                        <i
+                          className="fas fa-check"
+                          style={{ color: "green" }}
+                        ></i>
+                      ) : (
+                        <i
+                          className="fas fa-times"
+                          style={{ color: "red" }}
+                        ></i>
+                      )}
+                    </td>
+                    <td>
+                      <LinkContainer
+                        to={`/moderator/projects/${project._id}/edit`}
+                      >
+                        <Button variant="info" className="btn-sm">
+                          <i className="fas fa-edit"> </i>
+                        </Button>
+                      </LinkContainer>
+                    </td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        className="btn-sm"
+                        onClick={() => {
+                          deleteHandler(project._id);
+                        }}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </td>
+                    <td>
+                      <LinkContainer to={`/projects/${project._id}`}>
+                        <Button className="btn-sm" variant="info">
+                          Details
+                        </Button>
+                      </LinkContainer>
+                    </td>
+                  </tr>
+                )
+            )}
           </tbody>
         </Table>
       )}
