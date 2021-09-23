c2b07fc454d5c4ad7180434c5ccd6a28b1664471
i-iktear <emon5aurk@gmail.com>
2021-09-23 14:06:14 +0600

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import path from "path";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
@@ -15,17 +17,30 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Api is running.....");
-});
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/api/users", userRoutes);
 app.use("/api/users/moderator", moderatorRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/projects", ProjectRoutes);
 
-app.use(notFound);
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
